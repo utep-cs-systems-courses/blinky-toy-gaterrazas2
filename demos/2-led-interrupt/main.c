c3b9cf308d15b64df5519fe648f89eff0b4747dd
Gabriel Terrazas <Gaboon@Gabes-Mac.lan>
2023-07-07 15:35:42 -0600

    completed exercise demos 1 and 2

--- demos/2-led-interrupt/main.c
+++ demos/2-led-interrupt/main.c
@@ -4,20 +4,35 @@
 #include "led.h"
 
 int main(void) {
-  P1DIR |= LEDS;
-  P1OUT &= ~LED_GREEN;
-  P1OUT |= LED_RED;
+    P1DIR |= LEDS;
+    // Turns off Red LED
+    P1OUT &= ~LED_GREEN;
+    // Turns on green LED
+    P1OUT |= LED_RED;
 
-  configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
-  enableWDTInterrupts();	/* enable periodic interrupt */
-  
-  or_sr(0x18);		/* CPU off, GIE on */
+    configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
+    enableWDTInterrupts();	/* enable periodic interrupt */
+
+    or_sr(0x18);		/* CPU off, GIE on */
 }
 
 
 void
 __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
 {
-  P1OUT |= LED_GREEN;
-} 
+    // static Counter to keep track of interrupt occurrences without losing value
+    static unsigned int counter = 0;
+
+      if (counter < 5) {
+        // Toggle the red LED state using XOR operator
+        P1OUT ^= LED_GREEN;
+      }
+
+      counter++;
+
+      if (counter >= 10) {
+        // Reset the counter after reaching the desired number of interrupts
+        counter = 0;
+      }
+}
 
