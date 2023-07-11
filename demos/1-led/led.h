63aa4867ac2d16759f3c305f53339aa5dcd5fbce
Gabriel Terrazas <Gaboon@Gabes-Mac.lan>
2023-07-11 01:20:50 -0600

    demo #3

--- demos/1-led/led.h
+++ demos/1-led/led.h
@@ -1,8 +1,14 @@
 #ifndef led_included
 #define led_included
 
-#define LED_RED BIT0               // P1.0
-#define LED_GREEN BIT6             // P1.6
+#ifdef NEW_MSP_BOARD
+# define LED_GREEN BIT0               // P1.0
+# define LED_RED BIT6             // P1.6
+#else
+# define LED_RED BIT0               // P1.0
+# define LED_GREEN BIT6             // P1.6
+#endif
+
 #define LEDS (BIT0 | BIT6)
 
 #endif // included
--- demos/1-led/main.c
+++ demos/1-led/main.c
@@ -1,12 +1,18 @@
-//Alternate LEDs from Off, Green, and Red
+// Alternate LEDs from Off, Green, and Red
+// Include the MSP430 microcontroller header file
 #include <msp430.h>
+// Include the library for timer functions
 #include "libTimer.h"
+// Include the library for LED functions
 #include "led.h"
 
 int main(void) {
-  P1DIR |= LEDS;
-  P1OUT &= ~LED_GREEN;
-  P1OUT |= LED_RED;
+    // Set the direction of Port 1 pins as output for LEDs
+    P1DIR |= LEDS;
+    // Turn off the red LED by clearing the corresponding bit
+    P1OUT &= ~LED_RED;
+    // Turn on the  green LED by setting the corresponding bit
+    P1OUT |= LED_GREEN;
 
-  or_sr(0x18);		/* CPU off, GIE on */
+    or_sr(0x18);         
 }
